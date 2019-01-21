8879aaf803973d93e67bff8227eabb6f0dacef07
Arjan Nagi <a2nagi@uwaterloo.ca>
2019-01-21 11:26:42 -0500

    Add order CRUD functionality

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -2,5 +2,33 @@ const express = require('express');
 const router = express.Router();
 
 
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Orders fetched'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Order Created'
+    });
+});
+
+// Above, we just created an order
+
+router.get('/:orderID', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order Details',
+        orderID: req.params.orderID
+    });
+});
+
+router.delete('/:orderID', (req, res, next) => {
+    res.status(200).json({
+        message: 'Order Deleted',
+        orderID: req.params.orderID
+    });
+});
+
 
 module.exports = router;

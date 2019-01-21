f6b983a73de89ebe259759dd324eebb5c6ae8f98
Arjan Nagi <arjan@Arjans-MacBook-Pro.local>
2019-01-21 10:03:48 -0500

    Add features to run app

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require('express')
 const app = express();
 
-app.use()
+app.use((request, response, next) => {
+    response.status(200).json({
+        message: "It Works!"
+    });
+});
+
+module.exports = app;

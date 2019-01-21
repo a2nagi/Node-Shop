9bd77e7bb1b765bba1a9101d995461fe8da8415a
Arjan Nagi <arjan@Arjans-MacBook-Pro.local>
2019-01-21 10:38:24 -0500

    add CRUD

---
+++ api/routes/products.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: '/products GET requests reach here'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message: '/products POST requests reach here'
+    });
+});
+
+router.get('/:ID', (req, res, next) => {
+    const id = req.params.ID;
+    if (id == 'special')
+    {
+        res.status(200).json({
+            message: 'This is a special product',
+            id: id
+        });
+    }
+
+    else{
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+router.patch('/:ID', (req, res, next) => {
+    res.status(200).json({
+        message: 'Edited Product'
+    });
+});
+
+router.delete('/:ID', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted Product'
+    });
+});
+
+
+
+module.exports = router;
